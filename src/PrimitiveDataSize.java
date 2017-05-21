ccab9f6a682bd2a164d8956b08d21b16d0327cb7
Sudeep Yadav <Sudeep Yadav>
2017-05-21 15:52:23 +0530

    first commit

---
+++ src/PrimitiveDataSize.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Sudeep on 5/20/2017.
+ */
+public class PrimitiveDataSize {
+    public static int sizeof(int i){return 4;}
+    public static int sizeof(byte b){return 1;}
+    public static int sizeof(short s){return 2;}
+    public static int sizeof(long l){return 8;}
+}
---
+++ src/Test.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Sudeep on 5/17/2017.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+        int a=6;
+        short b=33;
+        byte c=36;
+        long d=344L;
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+        System.out.println("c=" + c);
+        System.out.println("d=" + d);
+
+        System.out.println();
+
+    }
+
+}
