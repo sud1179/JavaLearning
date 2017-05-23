ca4fa7c46f161f65fca640c8f423092699e28b07
Sudeep <sud1179@gmail.com>
2017-05-23 08:46:03 +0530

    new varibles

--- src/PrimitiveDataSize.java
+++ src/PrimitiveDataSize.java
@@ -6,4 +6,9 @@ public class PrimitiveDataSize {
     public static int sizeof(byte b){return 1;}
     public static int sizeof(short s){return 2;}
     public static int sizeof(long l){return 8;}
+    public static int sizeof(boolean bool){return 1;}
+    public static int sizeof(char c){return 1;}
+    public static int sizeof(float f){return 4;}
+    public static int sizeof(double d){return 8;}
+
 }
--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 /**
  * Created by Sudeep on 5/17/2017.
  */
-public class Test {
+public class Test extends PrimitiveDataSize {
     public static void main(String[] args){
         System.out.println("Hello World!!!");
         int a=6;
@@ -12,6 +12,19 @@ public class Test {
         System.out.println("b=" + b);
         System.out.println("c=" + c);
         System.out.println("d=" + d);
+
+        boolean bool= true;
+        char ch='C';
+        float f=230f;
+        double dbl=214.977d;
+
+        if(bool==true){
+            System.out.println("its boolen");
+        }
+        System.out.println("character="+ch);
+        System.out.println("float="+f);
+        System.out.println("Double"+dbl);
+
 
         System.out.println();
 
